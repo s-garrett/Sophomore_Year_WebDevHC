d0e5adab72339eef959436cf9706eaec72b66f64
s-garrett <sjg36@duke.edu>
2017-11-04 15:42:57 -0400

    Completed task 3 and mostof task 4. I reallly don't wanna do all but num

--- project3/code.js
+++ project3/code.js
@@ -1,6 +1,43 @@
 function equationSolver() {
   const input = document.getElementById("ex1").value;
   const x = Number(input);
-  const answer = `5*x - 3x^2 +13 = ${(5*x) - (3* (Math.pow(x,2))) + 13}`;
-  document.getElementById("task1").innerHTML = answer;
+  const answer0 = `5*x - 3x^2 +13 = ${(5*x) - (3* (Math.pow(x,2))) + 13}`;
+  document.getElementById("task1").innerHTML = answer0;
+}
+
+function combineStrings(){
+  const input0 = document.getElementById('string0').value;
+  const input1 = document.getElementById('string1').value;
+  const input2 = document.getElementById('string2').value;
+  const toCombine = ["Hello,", input0, input1, input2 + "!"];
+  const answer1 = toCombine.join(" ");
+  document.getElementById('task2').innerHTML =  answer1;
+}
+
+function XandY(){
+  const task3NumX = document.getElementById('x').value;
+  const task3NumY = document.getElementById('y').value;
+  if (task3NumX > task3NumY) {
+    document.getElementById('task3').innerHTML = "bananas".fontcolor("yellow");
+  }
+  else if (task3NumY > task3NumX) {
+    document.getElementById('task3').innerHTML = "porcupine".fontcolor("brown");
+  }
+  else if (task3NumX == task3NumY && task3NumX < 0) {
+    document.getElementById('task3').innerHTML = "red apples".fontcolor("red");
+  }
+  else {
+    document.getElementById('task3').innerHTML = "green apples".fontcolor("green");
+  }
+}
+
+function pNumbers(){
+  const pNumInput = document.getElementById('phoneNumber').value;
+  let Ans = pNumInput.replace(/-| /g, "");
+  if (Ans.length < 10 | Ans.length > 11 | Ans.charAt(0) != "1") {
+    document.getElementById('task4').innerHTML = "invalid".fontcolor("red");
+  }
+  else {
+      document.getElementById('task4').innerHTML = "valid".fontcolor("green");
+  }
 }
