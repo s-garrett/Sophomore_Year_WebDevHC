bba84722cb055b7b561b31ea691c61c18c4a7158
audreyrw59 <31712939+audreyrw59@users.noreply.github.com>
2017-11-27 20:51:41 -0500

    Added jquery source file + jquery for question 2

--- project4/scripts.js
+++ project4/scripts.js
@@ -3,5 +3,10 @@ $(document).ready(function(){
     $("#task1").innerHTML =
 
   })
-
+const input0 $("string0")val()
+const input1 $("string1")val()
+const input2 $("string2")val()
+      $("trigger2").click(function(){
+        $("task2").val(input0+input1+input2)
+      })
 })
