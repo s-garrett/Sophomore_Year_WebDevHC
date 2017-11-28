09bada8a5069e262fefc3b35e2a19e69b7d93d1f
s-garrett <sjg36@duke.edu>
2017-11-27 23:22:41 -0500

    Task 1 now works. Follow the same idea for other tasks.

--- project4/scripts.js
+++ project4/scripts.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-  $(".trigger1").click(function(){
-    $("#task1").innerHTML =
+$(document).ready(function() {
+  //task1
+  $(document).on('click', '.trigger1', function(){
+    var myInput = $("#ex1").val();
+    var ans =  (5 * myInput - Math.pow((3 * myInput), 2) + 13);
+    var outMe = '5 * myInput - Math.pow((3 * myInput), 2) + 13 = ' + ans;
+    $('#task1').text(outMe);
+  });
 
-  })
+  //task2
 
-})
+});
